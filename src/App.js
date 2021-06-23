65cd4fc74e1b4983627444f8d126a939478c22f9
unknown <oumaymamesbahi2@gmail.com>
2021-06-23 16:01:36 +0200

    props

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import Photo3 from './images/photo3.jpg';
 import Photo4 from './images/photo4.jpg';
 
     function App() {
-      const HandleAlert=(name)=>{
-        alert('Hi Im ${name}')
+      const HandleName=(name)=>{
+        alert(`Hi Im ${name}`)
       }
       return(
         <>
         <Com/>
-        <Profil name={'Albert Einstein'}   Bio='(born March 14, 1879, Ulm, Württemberg, Germany—died April 18, 1955, Princeton, New Jersey, U.S.), German-born physicist who developed the special and general theories of relativity and won the Nobel Prize for Physics in 1921 for his explanation of the photoelectric effect'  Profession='Physicist' HandleAlert={HandleAlert} >
-            <img  style={{ width: "300px", borderRadius: "15px",marginTop:"-33%",marginLeft:"-77%",height:"331px" }}src={Alb} alt="image"/>
+        <Profil name={'Albert Einstein'} Profession='Physicist'  Bio='(born March 14, 1879, Ulm, Württemberg, Germany—died April 18, 1955, Princeton, New Jersey, U.S.), German-born physicist who developed the special and general theories of relativity and won the Nobel Prize for Physics in 1921 for his explanation of the photoelectric effect'   HandleName={HandleName} >
+            <img  style={{ width: "300px", borderRadius: "15px",marginTop:"-29%",marginLeft:"-77%",height:"345px" }}src={Alb} alt="image"/>
            <div className="img">
              <img  style={{width:"350px",cursor:"pointer",width:"300px",margin:"10px 60px",border: "1px solid rgba(0, 0, 0, 0.171)"}} src={Photo1} alt="image"/>
             <img  style={{width:"300px",cursor:"pointer",width:"300px",margin:"10px 71px",border: "1px solid rgba(0, 0, 0, 0.171)"}} src={Photo3} alt="image"/>
--- src/profil/profil-component.js
+++ src/profil/profil-component.js
@@ -4,17 +4,18 @@ import React, { Children } from 'react';
 
 function Profilcomponent(props) {
     const styleP={textAlign:'center',color:'black',marginTop: '-13%', display: "block",marginRight:'10%',
-    fontSize: "21px",letterSpacing: "3px", lineHeight: "20px",width:"100%",backgroundColor: "#5fa07891"}
+    fontSize: "21px",letterSpacing: "3px", lineHeight: "20px",width:"100%",backgroundColor: "#5fa07891",paddingTop:"5%"}
     return(
         <>
         <div style={styleP}>
-            
-        <div style={{fontWeight:'bold',fontSize:"29px",marginTop:"142px",marginLeft:"-29%"}}>{props.name}</div>
+            <div>
+        <div style={{fontWeight:'bold',fontSize:"29px",marginTop:"153px",marginLeft:"-29%"}}>{props.name}</div>
+           <p className="phy">(Physicis)</p>
         <div style={{fontSize:"16px", marginLeft:"320px",width:"284px",marginTop:"45px"}}>{props.Bio}</div>
-        <div style={{marginTop:"83px",marginLeft:"-67%"}}>{props.Profession}</div> 
-        <button className="btn1"onClick={()=>props.HandleAlert(props.name)}>Click</button>
+        
+        <button className="btn1" onClick={()=>props.HandleName(props.name)}>Click</button>
         {props.children}
-       
+        </div>
         </div>
         </>
     );
